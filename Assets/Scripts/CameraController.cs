83c313a353b44d357bce21f67f7a3abcf9c1d13c
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2023-11-27 18:11:45 -0500

    CameraControllerV1.01

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,33 +1,70 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CameraController:MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public WebCamTexture webcamTexture;
+    public RawImage rawImage; // Asigna la imagen del Canvas en el Inspector
+
+    // Enum para representar la dirección de la cámara
+    public enum CameraFacingDirection
+    {
+        Front,
+        Back
+    }
+    private WebCamTexture webcamTexture;
 
     void Start()
     {
-        // Obtener todas las cámaras disponibles en el dispositivo
+        // Obtener una lista de dispositivos de cámara disponibles
         WebCamDevice[] devices = WebCamTexture.devices;
 
-        if (devices.Length > 0)
-        {
-            // Seleccionar la primera cámara disponible (puedes elegir otra si prefieres)
-            webcamTexture = new WebCamTexture(devices[0].name);
-            // Asignar la textura de la cámara a un material por ejemplo
-            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-            renderer.material.mainTexture = webcamTexture;
+        // Buscar la cámara posterior
+        WebCamDevice selectedCamera = SelectCamera(devices, CameraFacingDirection.Back);
 
-            // Iniciar la visualización de la cámara
+        // Si no se encontró la cámara posterior, buscar la cámara delantera
+        if (selectedCamera.name == null)
+        {
+            selectedCamera = SelectCamera(devices, CameraFacingDirection.Front);
+        }
+
+        // Verificar si se encontró alguna cámara disponible
+        if (selectedCamera.name != null)
+        {
+            // Crear una textura de cámara con la cámara seleccionada
+            webcamTexture = new WebCamTexture(selectedCamera.name);
+
+            // Redimensionar la textura de la cámara para que coincida con el tamaño de la imagen
+            webcamTexture.requestedWidth = (int)rawImage.rectTransform.rect.width;
+            webcamTexture.requestedHeight = (int)rawImage.rectTransform.rect.height;
+
+            // Asignar la textura de la cámara al RawImage del Canvas
+            if (rawImage != null)
+            {
+                rawImage.texture = webcamTexture;
+            }
+
+            // Comenzar la transmisión de la cámara
             webcamTexture.Play();
         }
         else
         {
-            Debug.Log("No se encontraron cámaras disponibles en el dispositivo.");
+            Debug.Log("No hay cámara disponible.");
         }
     }
 
-    void Update()
+    // Resto del código igual que antes...
+
+    // Función para buscar la cámara según la dirección especificada
+    private WebCamDevice SelectCamera(WebCamDevice[] devices, CameraFacingDirection facingDirection)
     {
-        // Aquí puedes agregar lógica adicional si necesitas interactuar con la entrada de la cámara en tiempo real
+        foreach (WebCamDevice device in devices)
+        {
+            if ((facingDirection == CameraFacingDirection.Front && device.isFrontFacing) ||
+                (facingDirection == CameraFacingDirection.Back && !device.isFrontFacing))
+            {
+                return device;
+            }
+        }
+        return new WebCamDevice(); // Devolver una instancia vacía si no se encuentra ninguna cámara
     }
 }
